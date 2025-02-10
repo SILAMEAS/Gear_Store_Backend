a95abce4e933ace49892e2af52364ae720def351
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-02-10 21:55:33 +0700

    ALLOWED_HOSTS = ['*']

--- gear_store/settings.py
+++ gear_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+0eqisb&ggsi+f+1&^qx7rgl8xjfxv1fc-53=s4o@bd&+p&xxn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
