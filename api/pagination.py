30827d5130870ed7f933952393e19522637ea961
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-02-19 23:47:54 +0700

    page pageSize

--- api/pagination.py
+++ api/pagination.py
@@ -4,17 +4,18 @@ from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'limit'
+    page_size_query_param = 'pageSize'
     page_query_param = 'page'
     max_page_size = 20
 
     def get_paginated_response(self, data):
         return Response({
             'hasNext': self.page.has_next(),
-            'currentPage': self.page.number,
+            'page': self.page.number,
             'total': self.page.paginator.count,
             'contents': data,
             'totalPages': self.page.paginator.num_pages,
+            'pageSize': self.page.paginator.per_page,  # Corrected this
         })
 
     # pagination_class = LimitOffsetPagination    # http://localhost:8000/products/?limit=2&offset=8
