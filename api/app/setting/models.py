4c47c00e95731a181b0ca7b768f91dae2c2f814d
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-03-23 16:37:29 +0700

    sunday

--- api/app/setting/models.py
+++ api/app/setting/models.py
@@ -7,12 +7,13 @@ class Settings(models.Model):
     ]
     logo = models.ImageField(upload_to='logo/', null=True, blank=True)
     mode = models.CharField(max_length=10, choices=MODE, default='light')
-    Primary = models.CharField(max_length=7, default="#D6056A")
-    Secondary = models.CharField(max_length=7, default="#800080")
-    Success = models.CharField(max_length=7, default="#28a745")
-    Warning = models.CharField(max_length=7, default="#ffc107")
-    Danger = models.CharField(max_length=7, default="#dc3545")
-    Info = models.CharField(max_length=7, default="#007bff")
+    primary = models.CharField(max_length=7, default="#D6056A")
+    secondary = models.CharField(max_length=7, default="#800080")
+    success = models.CharField(max_length=7, default="#28a745")
+    warning = models.CharField(max_length=7, default="#ffc107")
+    danger = models.CharField(max_length=7, default="#dc3545")
+    info = models.CharField(max_length=7, default="#007bff")
+    Grey = models.CharField(max_length=7, default="#787671")
 
     def __str__(self):
         return "Theme Colors Configuration"
